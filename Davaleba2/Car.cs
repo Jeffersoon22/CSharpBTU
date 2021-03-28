afc0623377d1f5c5009915d2640dd738aee70667
Natalie Japaridze <Jeffersoon22@users.noreply.github.com>
2021-03-28 21:35:08 +0400

    add half assignment2

---
+++ Davaleba2/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Davaleba2
+{
+    class Car
+    {
+        private string color;
+        private int numberdoors;
+        public string ownerLastName,brand;
+
+        public Car(string feri, int karebebi, string mflobelisgvari, string firma)
+        {
+            color = feri;
+            numberdoors = karebebi;
+            ownerLastName = mflobelisgvari;
+            brand = firma;
+        }
+
+        public void GetValues()
+        {
+            Console.WriteLine(color +"\n"+numberdoors);
+        }
+
+    }
+}
---
+++ Davaleba2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Davaleba2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double tevadoba = double.Parse(textBox1.Text);
+            double distancia = double.Parse(textBox2.Text);
+            Airplane airplane = new Airplane(tevadoba,distancia);
+            airplane.GetValues(label3);
+            label3.Text += $"\nდისტანცია რომელსაც სავსე ბაკით გაივლის: {airplane.Calcualte()} კმ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double result;
+
+            bool aisDouble = double.TryParse(textBox1.Text,out result);
+            bool bisDouble = double.TryParse(textBox2.Text,out result); 
+            bool cisDouble = double.TryParse(textBox3.Text,out result);
+            if (aisDouble && bisDouble && cisDouble)
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                double c = double.Parse(textBox3.Text);
+                Samkutxedi samkutxedi = new Samkutxedi(a, b, c);
+                samkutxedi.GetValues(label3);
+            }
+       
+
+        }
+
+    }
+}
