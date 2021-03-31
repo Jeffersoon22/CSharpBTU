df47459a267843b556e15038877680787896a3be
Natalie Japaridze <48613548+Jeffersoon22@users.noreply.github.com>
2021-03-31 21:07:13 +0400

    add full task4

---
+++ Davaleba4/MartkutxediBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Davaleba4
+{
+    class MartkutxediBase
+    {
+        protected double fudze;
+
+        public MartkutxediBase(double fudze)
+        {
+            this.fudze = fudze;
+        }
+
+        public string GetValue()
+        {
+            string result = $"ფუძე: {fudze}";
+            return result;
+        }
+    }
+    class GenMartkutxedi1 : MartkutxediBase
+    {
+        double height;
+
+        public GenMartkutxedi1(double fudze,double height) : base(fudze)
+        {
+            this.height = height;
+        }
+        public double GetArea()
+        {
+            return height * fudze;
+        }
+    }
+}
